5a84bb3e61002cd5a91c62c89d8c6602ee6735f9
Chris1320 <me@chrisfromnowhere.me>
2025-10-03 12:15:30 +0800

    build(android): add firebase-auth dependency
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- ReconnectED-Android/app/build.gradle.kts
+++ ReconnectED-Android/app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.firebase.auth)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
