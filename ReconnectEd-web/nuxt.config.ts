9a8a730d38c9281d44d6fede985dea16355a3010
Chris1320 <me@chrisfromnowhere.me>
2025-10-08 01:48:43 +0800

    refactor: remove unused Nuxt modules and update package dependencies
    - Removed "@nuxt/fonts" and "@nuxt/icon" from nuxt.config.ts modules.
    - Updated "nuxt" dependency to version "^4.1.3" in package.json.
    - Added "typescript" dependency to package.json.
    
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- ReconnectEd-web/nuxt.config.ts
+++ ReconnectEd-web/nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
-    modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/ui"],
+    modules: ["@nuxt/eslint", "@nuxt/ui"],
     css: ["~/assets/css/main.css"],
 });
