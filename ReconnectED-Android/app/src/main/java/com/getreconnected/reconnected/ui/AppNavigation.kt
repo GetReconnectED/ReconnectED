d6b48c1c49fc76df1d9079b3f85ec4151f4c049e
Chris1320 <me@chrisfromnowhere.me>
2025-10-16 18:53:47 +0800

    feat(android): move `UIRouteViewModel` to `AppNavigation.kt`
    Move `UIRouteViewModel` to `AppNavigation.kt` to allow it to be passed
    between screens.
    
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- ReconnectED-Android/app/src/main/java/com/getreconnected/reconnected/ui/AppNavigation.kt
+++ ReconnectED-Android/app/src/main/java/com/getreconnected/reconnected/ui/AppNavigation.kt
@@ -1,11 +1,13 @@
 package com.getreconnected.reconnected.ui
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.getreconnected.reconnected.core.AppDatabase
 import com.getreconnected.reconnected.core.models.Screens
+import com.getreconnected.reconnected.core.viewModels.UIRouteViewModel
 import com.getreconnected.reconnected.ui.screens.LoginScreen
 import com.getreconnected.reconnected.ui.screens.MainScreen
 import com.getreconnected.reconnected.ui.screens.SplashScreen
@@ -13,6 +15,7 @@ import com.getreconnected.reconnected.ui.screens.SplashScreen
 @Composable
 @Suppress("ktlint:standard:function-naming")
 fun AppNavigation(database: AppDatabase) {
+    val viewModel: UIRouteViewModel = viewModel() // Initialize the view model for UI routes
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screens.Splash.name) {
         composable(Screens.Splash.name) {
@@ -24,10 +27,10 @@ fun AppNavigation(database: AppDatabase) {
             })
         }
         composable(Screens.Login.name) {
-            LoginScreen(navController = navController)
+            LoginScreen(viewModel = viewModel, navController = navController)
         }
         composable(Screens.Dashboard.name) {
-            MainScreen()
+            MainScreen(viewModel)
         }
     }
 }
